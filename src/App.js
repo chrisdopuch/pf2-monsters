6134513c1861a865c8a396a5d78cc830fc57afa9
Chris Dopuch <chris.dopuch@gmail.com>
2018-09-04 16:59:43 -0700

    added buttons and left panel, todo state management

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import ReactTable from "react-table";
-import "react-table/react-table.css";
+import {
+  Button,
+  Glyphicon,
+  Panel
+} from 'react-bootstrap';
+import logo from './logo.svg';
 import monsters from './monsters.json'
 
+import './App.css';
+import "react-table/react-table.css";
+
 class App extends Component {
+  handleClick() {
+    // todo
+  }
+  renderSelectorCell() {
+    return (
+      <Button bsStyle="success" onClick={this.handleClick}>
+        <Glyphicon glyph="plus"/>
+      </Button>
+    );
+  }
   render() {
     return (
       <div className="App">
@@ -13,25 +29,42 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Pathfinder Playtest Encounter Builder</h1>
         </header>
-        <ReactTable
-          data={monsters}
-          columns={[
-            {
-              Header: "Name",
-              accessor: "name",
-            },
-            {
-              Header: "Level",
-              accessor: "level",
-            },
-            {
-              Header: "Source",
-              accessor: "source",
-            },
-          ]}
-          defaultPageSize={10}
-          className="-striped -highlight"
-        />
+        <div className="App-flex-wrap">
+          <section className="App-left-section">
+            <Panel>
+              <Panel.Heading>
+                <Panel.Title componentClass="h3">Encounter Info</Panel.Title>
+              </Panel.Heading>
+              <Panel.Body></Panel.Body>
+            </Panel>
+          </section>
+          <section className="App-right-section">
+            <ReactTable
+              data={monsters}
+              columns={[
+                {
+                  id: 'selector',
+                  Cell: this.renderSelectorCell,
+                  maxWidth: 75,
+                },
+                {
+                  Header: "Name",
+                  accessor: "name",
+                },
+                {
+                  Header: "Level",
+                  accessor: "level",
+                },
+                {
+                  Header: "Source",
+                  accessor: "source",
+                },
+              ]}
+              defaultPageSize={10}
+              className="-striped -highlight"
+            />
+          </section>
+        </div>
       </div>
     );
   }
